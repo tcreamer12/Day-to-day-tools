bf8c625b26da240a4c54fe3583c03efc270a63fb
Tyler Creamer <153393318+tcreamer22@users.noreply.github.com>
2024-06-03 17:51:38 -0500

    Update localadmintool.ps1

--- localadmintool.ps1
+++ localadmintool.ps1
@@ -1,4 +1,4 @@
 # Quick little script to remove users from the local admin group except the ones you want to keep.
 
 Get-LocalGroupMember -Group "Administrators" | Where-Object {$_.myName -ne "$env:computername\user" -and $_.myName -ne "domain\name"} 
-| Remove-LocalGroupMember -Group Administrator -ErrorAction SilentlyContinue
+| Remove-LocalGroupMember -Group Administrators -ErrorAction SilentlyContinue
