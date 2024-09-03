ba36e9401939a717a2231523a27a2041a846741a
Tyler Creamer <153393318+tcreamer22@users.noreply.github.com>
2024-09-03 15:44:20 -0500

    Update localadmintool.ps1

--- localadmintool.ps1
+++ localadmintool.ps1
@@ -1,4 +1,4 @@
-# Quick little script to remove users from the local admin group except the ones you want to keep on each device.
+# Quick little script to remove users from a machine's local admin group except the ones you want to keep on each device.
 # Honestly you could just create a GPO for this, but I wanted to do it with PS.
 
 Get-LocalGroupMember -Group "Administrators" | Where-Object {$_.myName -ne "$env:computername\user" -and $_.myName -ne "domain\name"} 
