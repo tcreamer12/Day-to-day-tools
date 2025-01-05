02eab4ad72822a8402bb1e34b39da7e06b826396
Tyler Creamer <153393318+tcreamer22@users.noreply.github.com>
2025-01-05 06:28:47 -0600

    Update availableDHCP.ps1

--- availableDHCP.ps1
+++ availableDHCP.ps1
@@ -1,2 +1,2 @@
-// This is something I personally need to use all the time because we run a /24 subnet.
+# This is something I personally need to use all the time because we run a /24 subnet.
 Get-DhcpServerv4ScopeStatistics -ComputerName server.domain.com
