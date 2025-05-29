e2d88a1426f0b4c035460e42ff3c3f1a44dd817a
Tyler Creamer <153393318+tcreamer12@users.noreply.github.com>
2025-05-29 08:29:16 -0500

    Update and rename allowblockdomainMSSecurity.ps1 to blockdomainDefenderO365.ps1
    reference

--- blockdomainDefenderO365.ps1
+++ blockdomainDefenderO365.ps1
@@ -3,3 +3,7 @@
 # To add/block through powershell you must sign in to your Tenant through ExchangeOnline.
 
 New-TenantAlllowBlockListItems -ListType Sender -Block -Entries "attackerdomain.net", "attacker@attackerdomain.net" -Expiration Date -NoExpiration [-Notes "Reported phishing attempt"]
+
+
+# This is what I usually use, but there are lots of different options on how to block and allow a domain:
+# https://learn.microsoft.com/en-us/defender-office-365/tenant-allow-block-list-email-spoof-configure?view=o365-worldwide
