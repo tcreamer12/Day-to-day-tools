4deabab0352d0f2a9ff82948e7788caf7b7caca7
Tyler Creamer <153393318+tcreamer12@users.noreply.github.com>
2025-08-12 19:43:35 -0500

    Create Add-DWORD-Regedit.ps1

---
+++ Add-DWORD-Regedit.ps1
@@ -0,0 +1,36 @@
+# An issue came up where I had to manually add a regedit to a certain area in the HKLM Registry. No way in hell am I doing that manually everytime.
+# So here is a small script to create or change a DWORD value. 
+# I used ChatGPT to help build the structuring of the script.
+
+# Define variables
+$RegPath = "HKLM:\Path\App"
+$ValueName = "Name of value"
+$ExpectedValue = 0x00001337
+
+# If RegPath does not exist, then create path.
+if (-not (Test-Path $RegPath)) {
+
+  New-Item -Path $RegPath -Force | Out-Null 
+}
+
+# Error handling logic. 
+try {
+  $CurrentValue = Get-ItemProperty -Path $RegPath -Name $ValueName -ErrorAction Stop | Select-Object -ExpandProperty $ValueName
+} catch {
+  $CurrentValue = $null # DWORD is missing. Therefore, make $CurrentValue NULL.
+}
+
+# Compare
+if ($null -eq $CurrentValue) {
+  # Create as DWORD if missing
+  New-ItemProperty -Path $RegPath -Name $ValueName -Value $ExpectedValue -PropertyType DWORD -Force | Out-Null
+  Write-Host "Registry value created as DWORD" # $Valuename = $ExpectedValue
+}
+
+elseif ($CurrentValue -ne $ExpectedValue) {
+# Update existing value if it's not the expected value of Value 0x00001337
+Set-ItemProperty -Path $RegPath -Name $ValueName -Value $ExpectedValue
+Writte-Host "Registry value updated" # $ValueName = $ExpectedValue
+} else {
+  Write-Host "Registry value already set correctly"
+}
